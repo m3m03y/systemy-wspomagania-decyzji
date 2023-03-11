6f99ac89bdbd3ce1a0f8b39267d71d16a2c6372a
Kinga Lipiszko <memoey99@gmail.com>
2023-03-11 14:12:43 +0100

    add second method - by alphabet

--- app/tools/core.py
+++ app/tools/core.py
@@ -1,6 +1,8 @@
 import typing as t
 
-def convert_text_to_numeric(word_list: t.List[str]):
+from numpy import copy
+
+def convert_text_to_numeric_by_presence(word_list: t.List[str]):
     convert_map = {}
     current_class = 1
     numeric_list = []
@@ -10,3 +12,21 @@ def convert_text_to_numeric(word_list: t.List[str]):
             current_class+=1
         numeric_list.append(convert_map[value]) 
     return numeric_list
+
+def convert_text_to_numeric_by_alphabet_order(word_list: t.List[str]):
+    alph_word_list = copy(word_list)
+    alph_word_list.sort()
+    convert_map = create_map(alph_word_list)
+    numeric_list = []
+    for value in word_list:
+        numeric_list.append(convert_map[value]) 
+    return numeric_list
+
+def create_map(word_list: t.List[str]):
+    index = 1
+    convert_map = {}
+    for word in word_list:
+        if word not in convert_map:
+            convert_map[word] = index
+            index+=1
+    return convert_map
--- test/core_test.py
+++ test/core_test.py
@@ -6,8 +6,23 @@ class Test_TestCoreMethods(unittest.TestCase):
 
    def test_convert_to_numeric_should_return_tree_numbers(self):
       word_list=["aaa", "bbb", "ccc"]
-      numeric_list = core.convert_text_to_numeric(word_list)
+      numeric_list = core.convert_text_to_numeric_by_presence(word_list)
       self.assertEqual(numeric_list, [1,2,3])    
+
+   def test_convert_to_numeric_should_return_with_valid_key(self):
+      word_list=["aaa", "bbb", "ccc", "bbb", "aaa"]
+      numeric_list = core.convert_text_to_numeric_by_presence(word_list)
+      self.assertEqual(numeric_list, [1,2,3,2,1])   
+
+   def test_convert_alphabetically_to_numeric_should_return_tree_numbers(self):
+      word_list=["aaa", "bbb", "ccc"]
+      numeric_list = core.convert_text_to_numeric_by_alphabet_order(word_list)
+      self.assertEqual(numeric_list, [1,2,3])    
+
+   def test_convert_mixed_alphabetically_to_numeric_should_return_tree_numbers(self):
+      word_list=["ccc", "aaa", "bbb"]
+      numeric_list = core.convert_text_to_numeric_by_alphabet_order(word_list)
+      self.assertEqual(numeric_list, [3,1,2]) 
 
 if __name__ == '__main__':
       unittest.main()
