d8489f48aa888385345fc78c468d80cfc3d5de80
Kinga Lipiszko <memoey99@gmail.com>
2023-03-24 20:41:28 +0100

    add input for discretization, plot refresh not working

--- app/gui/PlotHistogram.py
+++ app/gui/PlotHistogram.py
@@ -1,11 +1,13 @@
-from PyQt6.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QLabel, QComboBox, QHBoxLayout
+from PyQt6.QtWidgets import QDialog, QWidget, QCheckBox, QDialogButtonBox, QVBoxLayout, QSpinBox, QLabel, QComboBox, QHBoxLayout
 from PyQt6.QtCore import pyqtSignal as Signal
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from PyQt6.QtCore import Qt
+from tools.core import discretisation
+import pandas as pd
 class PlotHistogram(QDialog):
     columns_chosen = Signal(str, str)
 
@@ -50,20 +52,65 @@ class DisplayHistogram(QDialog):
         self.x_axis = x_name
         self.class_name = classes
         self.data = data
+        self.data_to_draw = self.data
         self.setWindowTitle('Histogram')
         self.figure = Figure(figsize=(5, 4), dpi=100)
         self.canvas = FigureCanvas(self.figure)
         toolbar = NavigationToolbar(self.canvas, self)
         layout = QVBoxLayout()
+        discretize_layout = QHBoxLayout()
+        self.enable_discretize = QCheckBox()
+        self.enable_discretize.stateChanged.connect(self.change_discretization_state)
+        self.discretize_input = QWidget()
+        self.discretize_input.setLayout(discretize_layout)
+        self.discretize_input.setDisabled(True)
+        self.division_number = QSpinBox()
+        self.division_number.setMinimum(1)
+        self.division_number.valueChanged.connect(self.plot_discretize)
+
+        layout.addWidget(self.enable_discretize)
+        discretize_layout.addWidget(QLabel("Choose division number:"))
+        discretize_layout.addWidget(self.division_number)
+
+        layout.addWidget(self.discretize_input)
         layout.addWidget(toolbar)
         layout.addWidget(self.canvas)
         self.setLayout(layout)
         self.plot()
+    
+    def change_discretization_state(self, s):
+        print(f'DisplayHistogram:: checked state: {s}')
+        if s == 0:
+            self.discretize_input.setDisabled(True)
+            self.division_number.setDisabled(True)
+            self.data_to_draw = self.data
+            self.plot()
+        else:
+            self.discretize_input.setEnabled(True)
+            self.division_number.setEnabled(True)
+            self.update()
+
+    def plot_discretize(self, i):
+        print(f'DisplayHistogram:: discretize with {i} divisions')
+        discretized_column = discretisation(
+            self.data[self.x_axis], i)
+        if discretized_column is None:
+            ...
+            #TODO: do something
+
+        df = self.data.copy()
+        df[self.x_axis] = discretized_column
+        print(f'DisplayHistogram:: current dataset: {df}')
+        self.data_to_draw = df
+        self.plot()
 
     def plot(self):
+        print(f'DisplayHistogram:: dataset to draw: {self.data_to_draw}')
         ax = self.figure.add_subplot(111)
 
         sns.set(style="darkgrid")
 
-        sns.histplot(data=self.data, x=self.x_axis, hue=self.class_name, kde=True, ax=ax)
+        sns.histplot(data=self.data_to_draw, x=self.x_axis, hue=self.class_name, kde=True, ax=ax)
+
+        self.update()
 
